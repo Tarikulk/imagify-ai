9ac39b8588dccee1048cc563ba556634ada8b1ca
Obayed12 <ullahtarik300@gmail.com>
2025-10-22 23:35:30 +0600

    auth page functional design logic added

--- src/components/authentication/AuthForm.tsx
+++ src/components/authentication/AuthForm.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import LoginForm from './LoginForm';
+
 
 const AuthForm = () => {
+
+  const [mode, setMode] = useState("login");
+
   return (
-    <div>AuthForm</div>
+    <div className='space-y-6'>
+      <div className="flex flex-col space-y-2 text-center">
+      <h1 className='text-2xl font-bold tracking-tight'> {
+          mode === "reset" ? "Reset Password" : mode === "login" ? "Login" : "Sign Up"
+        }</h1>
+
+<p className='text-sm text-muted-foreground'> {
+          mode === "reset" ? "Enter Your Email Below To Reset Your Password" : mode === "login" ? "Enter Your Email Below To Login In Your Account" : "Enter Your Information Below To Create An Account"
+        }</p>
+      </div>
+      {
+        mode === "login" && <LoginForm />
+      }
+      {
+        mode === "signup" && <span>Sign Up Form</span>
+      }
+      {
+        mode === "reset" && <span>Reset Password Form</span>
+      }
+    </div>
   )
 }
 
