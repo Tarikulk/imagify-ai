ff16089ae92305a99ca88bb14bef44ebd579dfb0
Obayed12 <ullahtarik300@gmail.com>
2025-11-01 23:25:57 +0600

    prompt ai formschema created

--- src/components/image-generation/Configurations.tsx
+++ src/components/image-generation/Configurations.tsx
@@ -11,7 +11,24 @@ import {z} from "zod"
 
 
 const formSchema = z.object({
-    username : z.string().min(2).max(50)
+   model : z.string({
+    required_error: "Model is required"
+   }),
+   prompt: z.string({
+    required_error: "Prompt is required"
+   }),
+   guidance: z.number({
+    required_error: "Guidance scale is required"
+   }),
+   num_outputs: z.number().min(1, {message: "Number of outputs should be atleast 1."}).max(1, {message: "Number of outputs must be less then 4."}),
+   aspect_ratio : z.string({
+    required_error: "Aspect ration is required"
+   }),
+   output_format: z.string({
+    required_error: "Out format is required"
+   }),
+   output_quality: z.number().min(1, {message: "output quality should be atleast 1."}).max(1, {message: "Output quality must be less then 100."}),
+   num_interfrence_steps : z.number().min(1, {message: "Number of inference step should be atleast 1."}).max(50, {message: "Number of inference step must be less then 4."}),
 })
 
 const Configurations = () => {
