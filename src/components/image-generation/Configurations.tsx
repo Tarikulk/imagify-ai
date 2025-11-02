4382351ab05e4e7bfc43d4e312c07da0c749188a
Obayed12 <ullahtarik300@gmail.com>
2025-11-02 23:02:14 +0600

    form component updated

--- src/components/image-generation/Configurations.tsx
+++ src/components/image-generation/Configurations.tsx
@@ -1,39 +1,100 @@
 "use client"
 import React from 'react'
 import {z} from "zod"
+import { zodResolver } from "@hookform/resolvers/zod" 
+import { useForm } from 'react-hook-form'
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
-// prompt: "black forest gateau cake spelling out the words \"FLUX 1 . 1 Pro\", tasty, food photography",
-// aspect_ratio: "1:1",
-// output_format: "webp",
-// output_quality: 80,
-// safety_tolerance: 2,
-// prompt_upsampling: true
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+ 
 
 
 const formSchema = z.object({
    model : z.string({
-    required_error: "Model is required"
+    message: "Model is required"
    }),
    prompt: z.string({
-    required_error: "Prompt is required"
+    message: "Prompt is required"
    }),
    guidance: z.number({
-    required_error: "Guidance scale is required"
+    message: "Guidance scale is required"
    }),
    num_outputs: z.number().min(1, {message: "Number of outputs should be atleast 1."}).max(1, {message: "Number of outputs must be less then 4."}),
    aspect_ratio : z.string({
-    required_error: "Aspect ration is required"
+    message: "Aspect ration is required"
    }),
    output_format: z.string({
-    required_error: "Out format is required"
+    message: "Out format is required"
    }),
    output_quality: z.number().min(1, {message: "output quality should be atleast 1."}).max(1, {message: "Output quality must be less then 100."}),
-   num_interfrence_steps : z.number().min(1, {message: "Number of inference step should be atleast 1."}).max(50, {message: "Number of inference step must be less then 4."}),
+   num_interfrence_steps : z.number().min(1, {message: "Number of inference step should be atleast 1."}).max(50, {message: "Number of inference step must be or equal to 50."}),
 })
 
 const Configurations = () => {
+
+      // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      model: "black-forest-labs/flux-1.1-pro",
+      prompt: "",
+      guidance: "3.5",
+      num_outputs: "1",
+      output_format: "jpg",
+      aspect_ratio: "1:1",
+      output_quality: 80,
+      num_interfrence_steps: 28,
+
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
-    <div>Configurations</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
